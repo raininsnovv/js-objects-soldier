84d2cc23af0a608b9093f5771cde5e1c5d5785d0
raininsnovv <raininsnovv@gmail.com>
2022-08-31 09:33:53 +0300

    Сделал subtask3 - code-review.

--- soldier.js
+++ soldier.js
@@ -1,30 +1,29 @@
 const soldier = {
-    name:Michael,
-    health:10,
-    weapon:{
-    model:'desert eagle',
-    cartridges:30
-    },
-    ammunitions:3,
-    fire(){
-    this.weapon.cartridges--
-    if(this.weapon.cartridges<=0){
-        return 'Обойма пуста. Перезарядитесь.'
+  name: "Michael",
+  health: 10,
+  weapon: {
+    model: "desert eagle",
+    cartridges: 30,
+  },
+  ammunitions: 3,
+  fire() {
+    this.weapon.cartridges--;
+    if (this.weapon.cartridges <= 0) {
+      return "Обойма пуста. Перезарядитесь.";
     }
-    },
-    reload(){
-
-    this.weapon.cartridges=30
-    this.ammunitions--
-    console.log('Перезарядка...')
-    if(this.ammunitions===0){
-        return 'Не осталось припасов,остались голые кулаки...'
+  },
+  reload() {
+    this.weapon.cartridges = 30;
+    this.ammunitions--;
+    console.log("Перезарядка...");
+    if (this.ammunitions === 0) {
+      return "Не осталось припасов,остались голые кулаки...";
     }
-    },
-    injure(){
-        this.health--
-        if(this.health<=0){
-            console.log('Ты проиграл...')
-        }
-    },
-}
+  },
+  injure() {
+    this.health--;
+    if (this.health <= 0) {
+      console.log("Ты проиграл...");
+    }
+  },
+};
