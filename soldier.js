c7e94f313c3bd596c832cd4f8a4da641209077f4
raininsnovv <raininsnovv@gmail.com>
2022-08-31 09:20:45 +0300

    Сделал subtask 1

---
+++ soldier.js
@@ -0,0 +1,18 @@
+const soldier = {
+    name:Michael,
+    health:10,
+    weapon:{
+    model:'desert eagle',
+    cartridges:30
+    },
+    ammunitions:3,
+    fire(){
+
+    },
+    reload(){
+
+    },
+    injure(){
+
+    },
+}
